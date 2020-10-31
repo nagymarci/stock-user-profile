687ff300baeb9d73d990c0ed9f0d330bc744de72
nagymarci <nagy90marci@gmail.com>
2020-10-31 09:24:08 +0100

    feat(userprofile): add default expectation and getter

--- model/userprofile.go
+++ model/userprofile.go
@@ -1,13 +1,28 @@
 package model
 
 type Expectation struct {
-	Stock         string  `bson:"stock"`
-	ExpectedRaise float64 `bson:"expectedRaise"`
+	Stock         string  `bson:"stock" json:"stock"`
+	ExpectedRaise float64 `bson:"expectedRaise" json:"expectedRaise"`
 }
 
 type Userprofile struct {
-	UserID         string        `bson:"_id"`
-	Email          string        `bson:"email"`
-	Expectations   []Expectation `bson:"expectations"`
-	ExpectedReturn float64       `bson:"expectedReturn"`
+	UserID             string        `bson:"_id" json:"userId"`
+	Email              string        `bson:"email" json:"email"`
+	Expectations       []Expectation `bson:"expectations" json:"expectations"`
+	ExpectedReturn     float64       `bson:"expectedReturn" json:"expectedReturn"`
+	DefaultExpectation float64       `bson:"defaultExpectations" json:"defaultExpectation"`
+}
+
+//GetExpectation return the expectation for the given stock if exists, returns
+// the default otherwise
+func (u *Userprofile) GetExpectation(stock string) float64 {
+	expectations := u.Expectations
+
+	for _, exp := range expectations {
+		if exp.Stock == stock {
+			return exp.ExpectedRaise
+		}
+	}
+
+	return u.DefaultExpectation
 }
